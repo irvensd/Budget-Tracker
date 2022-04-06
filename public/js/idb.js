1afbbe487668645bb76a8056fe9d8aa5afcdef75
Irvens R Dupuy <92906651+irvensd@users.noreply.github.com>
2022-04-06 14:38:27 -0400

    Deploying

--- public/js/idb.js
+++ public/js/idb.js
@@ -5,7 +5,7 @@ const request = indexedDB.open("budget_tracker", 1);
 request.onupgradeneeded = function(event) {
   // save a reference to the database 
   const db = event.target.result;
-  // create an object store (table) called `new_pizza`, set it to have an auto incrementing primary key of sorts 
+  
   db.createObjectStore('new_budget', { autoIncrement: true });
 };
 
