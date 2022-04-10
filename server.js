14620edda536b83a081d0353355eb83c5ea8c918
Irvens R Dupuy <92906651+irvensd@users.noreply.github.com>
2022-04-10 15:37:02 -0400

    Deploying

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3001;
+// const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
@@ -23,8 +23,8 @@ mongoose.connect(MONGODB_URI, {
 
 app.use(require("./routes/api.js"));
 
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+app.listen(process.env.PORT || 3003, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 // k6xB9XJthcrf9ke2
