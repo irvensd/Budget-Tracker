ab610a4722fd695edfd57a0d2e17c2bea5a1125a
Irvens R Dupuy <92906651+irvensd@users.noreply.github.com>
2022-04-10 15:30:15 -0400

    Deploying

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = process.env.PORT || 3003;
+const PORT = process.env.PORT || 3001;
 const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
@@ -18,14 +18,10 @@ app.use(express.static("public"));
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
-  useFindAndModify: true
+  useFindAndModify: false
 });
 
-// routes
 app.use(require("./routes/api.js"));
-
-// Use this to log mongo queries being executed!
-mongoose.set('debug', true);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
